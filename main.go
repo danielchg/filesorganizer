57d70535bf9ed4266f90c55e9e271738e599b04d
Daniel Chavero <dchavero@redhat.com>
2022-09-14 19:01:09 +0200

    add worker as goroutine

--- main.go
+++ main.go
@@ -3,39 +3,46 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
-	"sync"
+	"io/fs"
 
 	fm "github.com/danielchg/filesorganizer/pkg/filesmanagement"
 )
 
-var wg sync.WaitGroup
+func worker(id int, files_src_path, files_dest_path *string, jobs <-chan fs.FileInfo, result chan<- string) {
+	file := <-jobs
+	src := *files_src_path + "/" + file.Name()
+	fmt.Println("File to move: ", src, "by Worker: ", id)
 
-func ProcessingFiles(ch chan os.FileInfo, files_src_path string, files_dest_path string) {
-	defer wg.Done()
-	fl := <-ch
-	src := files_src_path + "/" + fl.Name()
-	fmt.Println("File to move: ", fl.Name())
-
-	dest, _ := fm.GetDestPath(fl, files_dest_path)
+	dest, _ := fm.GetDestPath(file, *files_dest_path)
 	fm.CreateNewPath(dest)
 
-	dest_file := dest + "/" + fl.Name()
+	dest_file := dest + "/" + file.Name()
 	fm.CopyFile(src, dest_file)
+
+	result <- dest_file
+
 }
 
 func main() {
-
 	files_src_path := flag.String("src", "./", "The path where there are the files to be organized")
 	files_dest_path := flag.String("dest", "./", "The path where the files will be organized")
 	flag.Parse()
 
-	files, _ := fm.ListFiles(*files_src_path)
-	ch := make(chan os.FileInfo)
-	for _, file := range files {
-		ch <- file
-		wg.Add(len(files))
-		go ProcessingFiles(ch, *files_src_path, *files_dest_path)
+	numWorkers := 3
+	jobs := make(chan fs.FileInfo)
+	result := make(chan string)
+	for w := 1; w <= numWorkers; w++ {
+		go worker(w, files_src_path, files_dest_path, jobs, result)
 	}
-	wg.Wait()
+
+	files, _ := fm.ListFiles(*files_src_path)
+	filesCount := len(files)
+	for _, file := range files {
+		jobs <- file
+	}
+
+	for a := 1; a <= filesCount; a++ {
+		<-result
+	}
+	close(jobs)
 }
