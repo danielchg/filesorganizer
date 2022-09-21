0982a72c56b92f9e9000088cadaf16c50ddf1b5c
Daniel Chavero <dchavero@redhat.com>
2022-09-21 18:19:28 +0200

    add error output to test

--- pkg/filesmanagement/filesmanagement_test.go
+++ pkg/filesmanagement/filesmanagement_test.go
@@ -17,7 +17,7 @@ func TestGetDestPath(t *testing.T) {
 	file, _ := os.Stat("../../samples/2019-01-27 06.08.07-1-2.jpg")
 	testedPath, _ := GetDestPath(file, "tests")
 	if testedPath != desiredPath {
-		t.Errorf("The destination path has not been generated correctly")
+		t.Errorf("The destination path has not been generated correctly, %v differ from %v", testedPath, desiredPath)
 	}
 
 }
