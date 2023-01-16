a1f9d8019bc1b2901dd4946a40ccce9157485692
Daniel Chavero <dchavero@redhat.com>
2023-01-16 19:13:42 +0100

    fix test

--- pkg/filesmanagement/filesmanagement_test.go
+++ pkg/filesmanagement/filesmanagement_test.go
@@ -1,8 +1,10 @@
 package filesmanagement
 
 import (
+	"fmt"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestListFiles(t *testing.T) {
@@ -14,6 +16,11 @@ func TestListFiles(t *testing.T) {
 
 func TestGetDestPath(t *testing.T) {
 	desiredPath := "tests/2019/January"
+	desiredTime := time.Date(2019, time.January, 27, 6, 7, 2, 0, time.UTC)
+	err := os.Chtimes("../../samples/2019-01-27 06.08.07-1-2.jpg", desiredTime, desiredTime)
+	if err != nil {
+		fmt.Println("Error chaning time of file")
+	}
 	file, _ := os.Stat("../../samples/2019-01-27 06.08.07-1-2.jpg")
 	testedPath, _ := GetDestPath(file, "tests")
 	if testedPath != desiredPath {
